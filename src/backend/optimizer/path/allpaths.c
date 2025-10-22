3389a51b90b2149de53ed643a8a2a5b9fa9b76db
Xuan Chen <xuan.chen@duke.edu>
2025-10-22 03:43:11 -0400

    - Fix the logs for base relations

--- src/backend/optimizer/path/allpaths.c
+++ src/backend/optimizer/path/allpaths.c
@@ -3306,27 +3306,17 @@ standard_join_search(PlannerInfo *root, int levels_needed, List *initial_rels) {
 
         ListCell *lc1;
         foreach(lc1, rel->pathlist) {
-            elog(LOG, "[path %d]", foreach_current_index(lc1));
+            elog(LOG, "[phase 0] [path %d]", foreach_current_index(lc1));
             const Path *path = lfirst(lc1);
             elog(LOG, "[path type] %d", path->pathtype);
-            const Sample *sample = path->total_cost_sample;
-            const int sample_count = sample->sample_count;
-            elog(LOG, "  [total cost avg] %.3f", path->total_cost);
-            for (int sample_idx = 0; sample_idx < sample_count; ++sample_idx) {
-                elog(LOG, "  [total cost %d] %.3f", sample_idx, sample->sample[sample_idx]);
-            }
+            elog(LOG, "  [exp cost] %.3f..%.3f", path->startup_cost, path->total_cost);
         }
 
         foreach(lc1, rel->partial_pathlist) {
-            elog(LOG, "[partial path %d]", foreach_current_index(lc1));
+            elog(LOG, "[phase 0] [partial path %d]", foreach_current_index(lc1));
             const Path *partial_path = lfirst(lc1);
             elog(LOG, "[partial path type] %d", partial_path->pathtype);
-            const Sample *sample = partial_path->total_cost_sample;
-            const int sample_count = sample->sample_count;
-            elog(LOG, "  [total cost avg] %.3f", partial_path->total_cost);
-            for (int sample_idx = 0; sample_idx < sample_count; ++sample_idx) {
-                elog(LOG, "  [total cost %d] %.3f", sample_idx, sample->sample[sample_idx]);
-            }
+            elog(LOG, "  [exp cost] %.3f..%.3f", partial_path->startup_cost, partial_path->total_cost);
         }
     }
 
